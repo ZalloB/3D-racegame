77af7f386a0778e85a28d1321e1dfcaf9c0a6433
Beatriz <beatriz.zallo@gmail.com>
2018-04-14 14:25:37 +0200

    inital commit

---
+++ Assets/Scripts/RoadController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RoadController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("dentrooooooooooo");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("fueraaaaaaaaaa");
+    }
+
+
+
+}
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour {
+
+    public GameObject car;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger " + other.tag);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision " + collision.collider.tag);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("Prueba" + hit.controller.tag);
+    }
+
+
+}
